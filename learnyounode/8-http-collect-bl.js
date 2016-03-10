ae7e634be494afb9205553ac51294b6102ae9690
Daryl Chan <dckj95@gmail.com>
2016-03-10 16:20:57 +0800

    Switched concatstream to bl for exercise 8

--- learnyounode/8-http-collect-bl.js
+++ learnyounode/8-http-collect-bl.js
@@ -1,10 +1,14 @@
 var http = require('http');
-var concatStream = require('concat-stream');
+var bl = require('bl');
 
 http.get(process.argv[2], collect);
 
 function collect(response) {
-  var store = concatStream(function(data) {
+  var store = bl(function(err, data) {
+    if (err) {
+      return console.error(err);
+    }
+    
     data = data.toString();
     
     console.log(data.length);
