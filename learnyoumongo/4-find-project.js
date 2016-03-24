68dddadea01e011c5287d460dd8aad36603e74a6
Daryl Chan <dckj95@gmail.com>
2016-03-24 13:15:10 +0800

    Completed learnyoumongo's exercise 4

---
+++ learnyoumongo/4-find-project.js
@@ -0,0 +1,19 @@
+var mongo = require('mongodb').MongoClient;
+
+var url = 'mongodb://localhost:27017/learnyoumongo';
+mongo.connect(url, (err, db) => {
+  if (err) throw err;
+  
+  var parrots = db.collection('parrots');
+  
+  parrots.find({
+    age: { $gt: parseInt(process.argv[2]) }
+  }, {
+    _id: 0
+  }).toArray((err, results) => {
+    if (err) throw err;
+    
+    console.log(results);
+    db.close();
+  });
+});
